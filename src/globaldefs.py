Fixed units, added 2nu probability plots

src/globaldefs.py
@@ -7,7 +7,7 @@ The core modules oscprob2nu.py and oscprob3nu.py do not require these
 constants.
 
 Created: 2019/04/17 17:03
-Last modified: 2019/04/23 18:27
+Last modified: 2019/04/27 16:55
 """
 
 
@@ -20,8 +20,7 @@ from numpy import *
 import numpy as np
 
 
-# CONV_KM_TO_INV_EV = 1./1.23984e-9 # [km^{-1} eV^{-1}]
-CONV_KM_TO_INV_EV = 8065.54e5
+CONV_KM_TO_INV_EV = 5.06773e9
 r"""float: Module-level constant
 
 Multiplicative conversion factor from km to eV^{-1}.
@@ -98,8 +97,12 @@ Average matter density in the Earth's crust.
 Units: [g cm^{-3}]
 """
 
+# NUM_DENSITY_E_EARTH_CRUST = DENSITY_MATTER_CRUST_G_PER_CM3 * CONV_G_TO_EV \
+#                             / ((MASS_PROTON+MASS_NEUTRON)/2.0) \
+#                             * ELECTRON_FRACTION_EARTH_CRUST \
+#                             / pow(CONV_CM_TO_INV_EV, 3.0)
 NUM_DENSITY_E_EARTH_CRUST = DENSITY_MATTER_CRUST_G_PER_CM3 * CONV_G_TO_EV \
-                            / ((MASS_PROTON+MASS_NEUTRON)/2.0) \
+                            / (MASS_PROTON+MASS_NEUTRON) \
                             * ELECTRON_FRACTION_EARTH_CRUST \
                             / pow(CONV_CM_TO_INV_EV, 3.0)
 r"""float: Module-level constant
@@ -115,7 +118,7 @@ Charged-current matter potential in the Earth's crust.
 Units: [eV]
 """
 
-S12_BF = sqrt(0.310)
+S12_BF = sqrt(0.310) # sqrt(0.32)
 r"""float: Module-level constant
 
 Lepton mixing angle sin(theta_12), best fit from NuFit 4.0, assuming
@@ -123,7 +126,7 @@ normal ordering with SK atmospheric data.
 Units: [Adimensional]
 """
 
-S23_BF = sqrt(0.582)
+S23_BF = sqrt(0.582) # sqrt(0.55)
 r"""float: Module-level constant
 
 Lepton mixing angle sin(theta_23), best fit from NuFit 4.0, assuming
@@ -131,7 +134,7 @@ normal ordering with SK atmospheric data.
 Units: [Adimensional]
 """
 
-S13_BF = sqrt(2.240e-2)
+S13_BF = sqrt(2.240e-2) # sqrt(0.022)
 r"""float: Module-level constant
 
 Lepton mixing angle sin(theta_13), best fit from NuFit 4.0, assuming
@@ -139,7 +142,7 @@ normal ordering with SK atmospheric data.
 Units: [Adimensional]
 """
 
-DCP_BF = 217./180.*np.pi
+DCP_BF = 217./180.*np.pi # -0.40*np.pi
 r"""float: Module-level constant
 
 Lepton CP-violation phase delta_CP, best fit from NuFit 4.0, assuming
@@ -147,7 +150,7 @@ normal ordering with SK atmospheric data.
 Units: [radian]
 """
 
-D21_BF = 7.39e-5
+D21_BF = 7.39e-5 # 7.50e-5
 r"""float: Module-level constant
 
 Mass-squared difference Delta m^2_21, best fit from NuFit 4.0, assuming
@@ -155,7 +158,7 @@ normal ordering with SK atmospheric data.
 Units: [eV^2]
 """
 
-D31_BF = 2.525e-3
+D31_BF = 2.525e-3 # 2.50e-3 + S12_BF*S12_BF*D21_BF
 r"""float: Module-level constant
 
 Mass-squared difference Delta m^2_31, best fit from NuFit 4.0, assuming
@@ -187,7 +190,7 @@ quark parameters compatible at 2sigma with LMA+coherent from 1805.04530.
 Units: [Adimensional]
 """
 
-EPS_MM = 0.0
+EPS_MM = 1.2
 r"""float: Module-level constant
 
 Total NSI strength parameter eps_mm computed using values of the u and d
@@ -195,7 +198,7 @@ quark parameters compatible at 2sigma with LMA+coherent from 1805.04530.
 Units: [Adimensional]
 """
 
-EPS_MT = -0.06
+EPS_MT = 0.0
 r"""float: Module-level constant
 
 Total NSI strength parameter eps_mt computed using values of the u and d
@@ -270,7 +273,7 @@ r"""float: Module-level constant
 LIV eigenvalue b_2.
 Units: [eV]
 """
-B3 = 5.e-9
+B3 = 2.e-9
 r"""float: Module-level constant
 
 LIV eigenvalue b_3.

src/hamiltonians2nu.py
@@ -90,7 +90,7 @@ def hamiltonian_2nu_vacuum_energy_independent(sth, Dm2,
     c2th = cos(2.0*th)
     s2th = sin(2.0*th)
 
-    f = 1./2.
+    f = 1./4.
 
     if not compute_matrix_multiplication:
 
@@ -111,6 +111,43 @@ def hamiltonian_2nu_vacuum_energy_independent(sth, Dm2,
         H = list(f*np.matmul(R, np.matmul(M2, matrix.transpose(R))))
 
     return H
+
+
+def probabilities_2nu_vacuum_std(sth, Dm2, energy, L):
+    r"""Returns 2nu oscillation vacuum probabilities, std. computation.
+
+    Returns the probabilities for two-neutrino oscillations in vacuum,
+    computed using the standard analytical expression of the
+    probabilities.
+
+    Parameters
+    ----------
+    sth : float
+        Sin(theta).
+    Dm2 : float
+        Mass-squared difference Delta m^2.
+    energy : float
+        Neutrino energy.
+    L : float
+        Baseline.
+
+    Returns
+    -------
+    list
+        List of probabilities [Pee, Pem, Pme, Pmm].
+    """
+    arg = 1.27*Dm2*L/energy#/4.0
+    cth = sqrt(1.0-sth*sth)
+    s2th = 2.0*sth*cth
+
+    Pem = s2th*s2th * pow(sin(arg), 2.0)
+    Pme = Pem
+    Pee = 1.0-Pem
+    Pmm = 1.0-Pme
+
+    prob = [Pee, Pem, Pme, Pmm]
+
+    return prob
 
 
 def hamiltonian_2nu_matter(h_vacuum_energy_independent, energy, VCC):
@@ -139,10 +176,58 @@ def hamiltonian_2nu_matter(h_vacuum_energy_independent, energy, VCC):
     h_matter = cp.deepcopy(h_vacuum_energy_independent)
     h_matter = np.multiply(1.0/energy, h_matter)
 
-    # Add the matter potential to the ee term to find the matter Hamiltonian
+    # Add the matter potential to the ee term to find the matter
+    # Hamiltonian
     h_matter[0][0] += VCC
 
     return h_matter
+
+
+def probabilities_2nu_matter_std(sth, Dm2, VCC, energy, L):
+    r"""Returns 2nu oscillation matter probabilities, std. computation.
+
+    Returns the probabilities for two-neutrino oscillations in matter,
+    computed using the standard analytical expression of the
+    probabilities.
+
+    Parameters
+    ----------
+    sth : float
+        Sin(theta).
+    Dm2 : float
+        Mass-squared difference Delta m^2.
+    VCC : float
+        Potential due to charged-current interactions of nu_e with
+        electrons.
+    energy : float
+        Neutrino energy.
+    L : float
+        Baseline.
+
+    Returns
+    -------
+    list
+        List of probabilities [Pee, Pem, Pme, Pmm].
+    """
+    x = 2.0*VCC*(energy*1.e9)/Dm2
+    cth = sqrt(1.0-sth*sth)
+    s2th = 2.0*sth*cth
+    s2thsq = s2th*s2th
+    c2th = sqrt(1.0-s2thsq)
+
+    Dm2m = Dm2*sqrt(s2thsq+pow(c2th-x, 2.0))
+    s2thmsq = s2thsq / (s2thsq+pow(c2th-x, 2.0))
+
+    arg = 1.27*Dm2m*L/energy#/4.0
+
+    Pem = s2thmsq * pow(sin(arg), 2.0)
+    Pme = Pem
+    Pee = 1.0-Pem
+    Pmm = 1.0-Pme
+
+    prob = [Pee, Pem, Pme, Pmm]
+
+    return prob
 
 
 def hamiltonian_2nu_nsi(h_vacuum_energy_independent, energy, VCC, eps):

src/hamiltonians3nu.py
@@ -205,7 +205,7 @@ def J(U, alpha, beta, k, j):
     return np.conj(U[alpha][k])*U[beta][k]*U[alpha][j]*np.conj(U[beta][j])
 
 
-def probabilities_3nu_std(U, D21, D31, energy, L):
+def probabilities_3nu_vacuum_std(U, D21, D31, energy, L):
     r"""Returns 3nu oscillation vacuum probabilities, std. computation.
 
     Returns the probabilities for three-neutrino oscillations in vacuum,
@@ -232,9 +232,9 @@ def probabilities_3nu_std(U, D21, D31, energy, L):
         Ptt].
     """
     D32 = D31-D21
-    arg21 = D21*L/energy/2.0
-    arg31 = D31*L/energy/2.0
-    arg32 = D32*L/energy/2.0
+    arg21 = 2.54*D21*L/energy#/2.0
+    arg31 = 2.54*D31*L/energy#/2.0
+    arg32 = 2.54*D32*L/energy#/2.0
     s21 = sin(arg21)
     s31 = sin(arg31)
     s32 = sin(arg32)
@@ -287,6 +287,111 @@ def hamiltonian_3nu_matter(h_vacuum_energy_independent, energy, VCC):
     h_matter[0][0] += VCC
 
     return h_matter
+
+
+def probabilities_3nu_matter_zs(U, D21, D31, energy, L):
+    r"""Returns 3nu oscillation vacuum probabilities, std. computation.
+
+    Returns the probabilities for three-neutrino oscillations in vacuum,
+    computed using the standard analytical expression of the
+    probabilities.
+
+    Parameters
+    ----------
+    U : list
+        3x3 PMNS complex mixing matrix.
+    D21 : float
+        Mass-squared difference Delta m^2_21.
+    D31 : float
+        Mass-squared difference Delta m^2_31.
+    energy : float
+        Neutrino energy.
+    L : float
+        Baseline.
+
+    Returns
+    -------
+    list
+        List of probabilities [Pee, Pem, Pet, Pme, Pmm, Pmt, Pte, Ptm,
+        Ptt].
+    """
+    s13sq = s13*s13
+    c13sq = 1.-s13sq
+    s12sq = s12*s12
+    c12sq = 1.-s12sq
+    s23sq = s23*s23
+    c23sq = 1.-s23sq
+    cosdCP = cos(dCP)
+    expdCP = complex(cos(dCP), sin(dCP))
+    expmdCP = complex(cos(dCP), -sin(dCP))
+
+    A = D21 + D31 + a
+    B = Dm21*D31 + a*(D31*c13sq+D21*(c13sq*c12sq+s13sq))
+    C = a*D21*D31*c13sq*c12sq
+    A2 = A*A
+    t1 = 2.*A2*A-9.*A*B+27.*C
+    t2 = 2.*pow(A2-3.*B, 1.5)
+    S = cos(arccos(t1/t2)/3.)
+
+    t3 = A/3.
+    t4 = sqrt(A*A-3.*B)*S
+    t5 = sqrt(1.-S*S)
+    t6 = sqrt(3.)/3.*t4*t5
+
+    m1sq = t3 - t4/3. - t6
+    m2sq = t3 - t4/3. + t6
+    m3sq = t3 + 2./3.*t4
+
+    D21m = 2.*t6
+    D31m = t4+t6
+    D32m = D31m-D21m
+
+    alpha = D31*c13sq + D21*(c13sq*c12sq+s13sq)
+    beta = D21*D31*c13sq*c12sq
+    E = (D31*(m3sq-D21) - D21*(m3sq-D31)*s12sq)*c13*s13
+    F = D21*(m3sq-D31)*c12*s12*c13
+
+    t7 = - (m2sq*m2sq - alpha*m2sq + beta)*D31m
+    t8 = D32m*(m1sq*m1sq - alpha*m1sq + beta) \
+            - D31m*(m2sq*m2sq - alpha*m2sq + beta)
+    s12sqm = t7/t8
+
+    t9 = m3sq*m3sq - alpha*m3sq + beta
+    t10 = D31m*D32m
+    s13sqm = t9/t10
+
+    t11 = E*E*s23sq + F*F*c23sq + 2.*E*F*c23*s23*cosdCP
+    t12 = E*E+F*F
+    s23sqm = t11/t12
+
+    t13 = (E*E*expmdCP - F*F*expdCP)*c23*s23 + E*F*(c23sq-s23sq)
+    t14 = E*E*s23sq + F*F*c23sq + 2.0*E*F*c23*s23*cosdCP
+    t15 = E*E*c23sq + F*F*s23sq - 2.0*E*F*c23*s23*cosdCP
+    expmdCPm = t13 / sqrt(t14*t15)
+
+
+    D32 = D31-D21
+    arg21 = D21*L/energy/2.0
+    arg31 = D31*L/energy/2.0
+    arg32 = D32*L/energy/2.0
+    s21 = sin(arg21)
+    s31 = sin(arg31)
+    s32 = sin(arg32)
+    ss21 = pow(sin(arg21/2.0), 2.0)
+    ss31 = pow(sin(arg31/2.0), 2.0)
+    ss32 = pow(sin(arg32/2.0), 2.0)
+
+    # Pee, Pem, Pet, Pme, Pmm, Pmt, Pte, Ptm, Ptt
+    prob = [delta(alpha, beta) \
+            - 4.0 * ( J(U, alpha, beta, 1, 0).real*ss21
+                    + J(U, alpha, beta, 2, 0).real*ss31
+                    + J(U, alpha, beta, 2, 1).real*ss32 ) \
+            + 2.0 * ( J(U, alpha, beta, 1, 0).imag*s21
+                    + J(U, alpha, beta, 2, 0).imag*s31
+                    + J(U, alpha, beta, 2, 1).imag*s32 ) \
+            for alpha in [0,1,2] for beta in [0,1,2]]
+
+    return prob
 
 
 def hamiltonian_3nu_nsi(h_vacuum_energy_independent, energy, VCC, eps):

src/oscprob3nu.py
@@ -42,6 +42,12 @@ import cmath
 import cmath as cmath
 
 
+SQRT3 = sqrt(3.0)
+r"""float: Module-level constant
+
+Constant equal to sqrt(3.0).
+"""
+
 SQRT3_INV = 1./sqrt(3.0)
 r"""float: Module-level constant
 
@@ -105,7 +111,7 @@ def hamiltonian_3nu_coefficients(hamiltonian_matrix):
     h5 = -H13.imag
     h6 = H23.real
     h7 = -H23.imag
-    h8 = (H11+H22-2.0*H33)*sqrt(3.0)/6.0
+    h8 = (H11+H22-2.0*H33)*SQRT3/6.0
 
     return [h1, h2, h3, h4, h5, h6, h7, h8]
 
@@ -281,7 +287,7 @@ def psi_roots(h2, h3):
         The three roots [psi1, psi2, psi3]
     """
     pre = 2.0*sqrt(h2)*SQRT3_INV
-    chi = cmath.acos(-sqrt(3.0)*h3*pow(h2,-1.5))
+    chi = cmath.acos(-SQRT3*h3*pow(h2,-1.5))
 
     roots = [pre*cmath.cos((chi+2.*np.pi*m)/3.0) for m in [1,2,3]]
 
@@ -384,9 +390,9 @@ def evolution_operator_3nu(hamiltonian_matrix, L):
         evolution_operator_3nu_u_coefficients(hamiltonian_matrix, L)
 
     evolution_operator = [
-                            [u0+1.j*(u3+u8/sqrt(3.)), 1.j*u1+u2, 1.j*u4+u5],
-                            [1.j*u1-u2, u0-1.j*(u3-u8/sqrt(3.)), 1.j*u6+u7],
-                            [1.j*u4-u5, 1.j*u6-u7, u0-1.j*2.*u8/sqrt(3.)]
+                            [u0+1.j*(u3+u8/SQRT3), 1.j*u1+u2, 1.j*u4+u5],
+                            [1.j*u1-u2, u0-1.j*(u3-u8/SQRT3), 1.j*u6+u7],
+                            [1.j*u4-u5, 1.j*u6-u7, u0-1.j*2.*u8/SQRT3]
     ]
 
     return evolution_operator

test/oscprob2nu_plotpaper.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-r"""Produce the plot of 3nu probabilities vs. energy shown in the paper.
+r"""Produce the plot of 2nu probabilities vs. energy shown in the paper.
 
-Contains a routine to generate and save the plot of three-neutrino
+Contains a routine to generate and save the plot of two-neutrino
 probabilities vs. energy that is included in the paper.
 
 Routine listings
 ----------------
 
-    * plot_probability_3nu_vs_energy_compare - Generates, saves the plot
+    * plot_probability_2nu_vs_energy_compare - Generates, saves the plot
 
 References
 ----------
@@ -15,8 +15,8 @@ References
 .. [1] Mauricio Bustamante, "Exact neutrino oscillation probabilities
    with arbitrary time-independent Hamiltonians", arXiv:1904.XXXXX.
 
-Created: 2019/04/17 18:08
-Last modified: 2019/04/22 20:36
+Created: 2019/04/26 21:20
+Last modified: 2019/04/26 21:20
 """
 
 __version__ = "0.1"
@@ -33,16 +33,16 @@ import matplotlib as mpl
 import sys
 sys.path.append('../src')
 
-import oscprob3nu
-import hamiltonians3nu
+import oscprob2nu
+import hamiltonians2nu
 from globaldefs import *
 
 
-def plot_probability_3nu_vs_energy_compare(output_format='pdf',
+def plot_probability_2nu_vs_energy_compare(output_format='pdf',
     output_path='./fig/'):
-    r"""Generates and saves a plot of 3nu probabilities vs. energy.
+    r"""Generates and saves a plot of 2nu probabilities vs. energy.
 
-    Generates and saves a plot of three-neutrino probabilities vs.
+    Generates and saves a plot of two-neutrino probabilities vs.
     energy for oscillations in vacuum, matter, with NSI, and with
     CPT-odd LIV.  This is the same plot that is included in the paper.
 
@@ -63,7 +63,7 @@ def plot_probability_3nu_vs_energy_compare(output_format='pdf',
 
     # Neutrino energies
     log10_energy_nu_min = log10(5.e-1) # [GeV]
-    log10_energy_nu_max = log10(3.e1) # [GeV]
+    log10_energy_nu_max = log10(4.e1) # [GeV]
     log10_energy_nu_npts = 400
     log10_energy_nu = np.linspace(  log10_energy_nu_min,
                                     log10_energy_nu_max,
@@ -83,43 +83,43 @@ def plot_probability_3nu_vs_energy_compare(output_format='pdf',
     fig.subplots_adjust(hspace=0.05, wspace=0.05)
 
     h_vacuum_energy_indep = \
-        hamiltonians3nu.hamiltonian_3nu_vacuum_energy_independent(  S12_BF,
-                                                                    S23_BF,
-                                                                    S13_BF,
-                                                                    DCP_BF,
-                                                                    D21_BF,
+        hamiltonians2nu.hamiltonian_2nu_vacuum_energy_independent(  S23_BF,
                                                                     D31_BF)
 
-    prob_vacuum = [oscprob3nu.probabilities_3nu( \
+    prob_vacuum = [oscprob2nu.probabilities_2nu( \
                     np.multiply(1./x/1.e9, h_vacuum_energy_indep), l) \
                     for x in energy_nu]
 
     # Uncomment to compare to the probability computed with the standard
     # ocillation formula in vacuum
-    # U = hamiltonians3nu.pmns_mixing_matrix(S12_BF, S23_BF, S13_BF, DCP_BF)
-    # prob_vacuum_std = [hamiltonians3nu.probabilities_3nu_vacuum_std( \
-    #                     U, D21_BF, D31_BF, x, l/CONV_KM_TO_INV_EV) \
+    # prob_vacuum_std = [hamiltonians2nu.probabilities_2nu_vacuum_std( \
+    #                     S23_BF, D31_BF, x, l/CONV_KM_TO_INV_EV) \
     #                 for x in energy_nu]
 
-    prob_matter = [oscprob3nu.probabilities_3nu( \
-                    hamiltonians3nu.hamiltonian_3nu_matter( \
+    prob_matter = [oscprob2nu.probabilities_2nu( \
+                    hamiltonians2nu.hamiltonian_2nu_matter( \
                         h_vacuum_energy_indep, x*1.e9, VCC_EARTH_CRUST), l)
                     for x in energy_nu]
 
-    # eps = eps_ee, eps_em, eps_et, eps_mm, eps_mt, eps_tt
-    prob_nsi = [oscprob3nu.probabilities_3nu( \
-                    hamiltonians3nu.hamiltonian_3nu_nsi( \
-                        h_vacuum_energy_indep, x*1.e9, VCC_EARTH_CRUST, EPS_3),
+    # Uncomment to compare to the probability computed with the standard
+    # ocillation formula in matter
+    # prob_matter_std = [hamiltonians2nu.probabilities_2nu_matter_std( \
+    #                     S23_BF, D31_BF, VCC_EARTH_CRUST, x, l/CONV_KM_TO_INV_EV) \
+    #                 for x in energy_nu]
+
+    prob_nsi = [oscprob2nu.probabilities_2nu( \
+                    hamiltonians2nu.hamiltonian_2nu_nsi( \
+                        h_vacuum_energy_indep, x*1.e9, VCC_EARTH_CRUST, EPS_2),
                         l)
                 for x in energy_nu]
 
-    prob_liv = [oscprob3nu.probabilities_3nu( \
-                    hamiltonians3nu.hamiltonian_3nu_liv( \
-                        h_vacuum_energy_indep, x*1.e9, SXI12, SXI23, SXI13,
-                        DXICP, B1, B2, B3, LAMBDA), l)
+    prob_liv = [oscprob2nu.probabilities_2nu( \
+                    hamiltonians2nu.hamiltonian_2nu_liv( \
+                        h_vacuum_energy_indep, x*1.e9, SXI12, B1, B3, LAMBDA),
+                        l)
                 for x in energy_nu]
 
-    # Pee, Pem, Pet, Pme, Pmm, Pmt, Pte, Ptm, Ptt
+    # Pee, Pem, Pmm
     for i, ax in enumerate(np.array(axes).reshape((1,3))[0]):
 
         if (i == 0): # Pee
@@ -127,6 +127,7 @@ def plot_probability_3nu_vs_energy_compare(output_format='pdf',
             p_vacuum = [x[0] for x in prob_vacuum]
             # p_vacuum_std = [x[0] for x in prob_vacuum_std]
             p_matter = [x[0] for x in prob_matter]
+            # p_matter_std = [x[0] for x in prob_matter_std]
             p_nsi = [x[0] for x in prob_nsi]
             p_liv = [x[0] for x in prob_liv]
 
@@ -134,21 +135,23 @@ def plot_probability_3nu_vs_energy_compare(output_format='pdf',
 
         elif (i == 1): # Pme
 
-            p_vacuum = [x[3] for x in prob_vacuum]
-            # p_vacuum_std = [x[3] for x in prob_vacuum_std]
-            p_matter = [x[3] for x in prob_matter]
-            p_nsi = [x[3] for x in prob_nsi]
-            p_liv = [x[3] for x in prob_liv]
+            p_vacuum = [x[2] for x in prob_vacuum]
+            # p_vacuum_std = [x[2] for x in prob_vacuum_std]
+            p_matter = [x[2] for x in prob_matter]
+            # p_matter_std = [x[2] for x in prob_matter_std]
+            p_nsi = [x[2] for x in prob_nsi]
+            p_liv = [x[2] for x in prob_liv]
 
             ylabel = r'$P_{\nu_\mu \to \nu_e}$'
 
         elif (i == 2): # Pmm
 
-            p_vacuum = [x[4] for x in prob_vacuum]
-            # p_vacuum_std = [x[4] for x in prob_vacuum_std]
-            p_matter = [x[4] for x in prob_matter]
-            p_nsi = [x[4] for x in prob_nsi]
-            p_liv = [x[4] for x in prob_liv]
+            p_vacuum = [x[3] for x in prob_vacuum]
+            # p_vacuum_std = [x[3] for x in prob_vacuum_std]
+            p_matter = [x[3] for x in prob_matter]
+            # p_matter_std = [x[3] for x in prob_matter_std]
+            p_nsi = [x[3] for x in prob_nsi]
+            p_liv = [x[3] for x in prob_liv]
 
             ylabel = r'$P_{\nu_\mu \to \nu_\mu}$'
             ax.set_xlabel(r'Neutrino energy [GeV]', fontsize=27)
@@ -161,6 +164,8 @@ def plot_probability_3nu_vs_energy_compare(output_format='pdf',
             # label=r'Vacuum std.')
         ax.plot(energy_nu, p_matter, color='C1', ls='--', lw=3.0, zorder=1,
             label=r'Matter')
+        # ax.plot(energy_nu, p_matter_std, color='b', ls='--', lw=3.0, zorder=1,
+            # label=r'Matter std.')
         ax.plot(energy_nu, p_nsi, color='C2', ls=':', lw=3.0, zorder=1,
             label=r'NSI')
         ax.plot(energy_nu, p_liv, color='C3', ls='-.', lw=3.0, zorder=1,
@@ -182,25 +187,26 @@ def plot_probability_3nu_vs_energy_compare(output_format='pdf',
         if (i == 0):
 
             ax.set_xticklabels([])
-            ax_yticks_major = np.array([0.85, 0.90, 0.95, 1.00])
+            ax_yticks_major = np.array([0.00, 0.25, 0.50, 0.75, 1.00])
             ax.set_yticks(ax_yticks_major, minor=False)
-            ax_yticks_minor = np.array([0.86, 0.87, 0.88, 0.89,
-                                        0.91, 0.92, 0.93, 0.94, 0.96, 0.97,
-                                        0.98, 0.99])
+            ax_yticks_minor = np.array([0.05, 0.10, 0.15, 0.20, 0.30, 0.35,
+                                        0.40, 0.45, 0.55, 0.60, 0.65, 0.70,
+                                        0.80, 0.85, 0.90, 0.95])
             ax.set_yticks(ax_yticks_minor, minor=True)
-            ax.set_ylim([0.85, 1.00])
+            ax.set_ylim([0.0, 1.0])
             ax.legend(loc='lower right', ncol=1, frameon=False,
                 columnspacing=1.)
 
         elif (i == 1):
 
             ax.set_xticklabels([])
-            ax_yticks_major = np.array([0.00, 0.05, 0.10])
+            ax_yticks_major = np.array([0.00, 0.25, 0.50, 0.75])
             ax.set_yticks(ax_yticks_major, minor=False)
-            ax_yticks_minor = np.array([0.01, 0.02, 0.03, 0.04, 0.06, 0.07,
-                                        0.08, 0.09, 0.11, 0.12])
+            ax_yticks_minor = np.array([0.05, 0.10, 0.15, 0.20, 0.30, 0.35,
+                                        0.40, 0.45, 0.55, 0.60, 0.65, 0.70,
+                                        0.80, 0.85, 0.90, 0.95])
             ax.set_yticks(ax_yticks_minor, minor=True)
-            ax.set_ylim([0.00, 0.12])
+            ax.set_ylim([0.0, 1.])
 
         elif (i == 2):
 
@@ -212,11 +218,11 @@ def plot_probability_3nu_vs_energy_compare(output_format='pdf',
             ax.set_yticks(ax_yticks_minor, minor=True)
             ax.set_ylim([0.0, 1.0])
 
-        pylab.savefig(output_path+'prob_3nu_vs_energy_compare.'+output_format,
+        pylab.savefig(output_path+'prob_2nu_vs_energy_compare.'+output_format,
             bbox_inches='tight', dpi=300)
 
     return
 
-plot_probability_3nu_vs_energy_compare( output_format='pdf',
+plot_probability_2nu_vs_energy_compare( output_format='pdf',
                                         output_path='../fig/')
 